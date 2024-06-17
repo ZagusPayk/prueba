4efadaa6efa1f9c64d1439b4be5639b61fc78912
ZagusPayk <167822643+ZagusPayk@users.noreply.github.com>
2024-06-17 09:30:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form')
+const nombres = document.getElementById('nombres')
+const email = document.getElementById('email')
+const mensaje = document.getElementById('mensaje')
+const mensaje2 = document.getElementById('mensaje2')
+
